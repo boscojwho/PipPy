45df5ab592b74d326a0c117ccd326e9d0b8d3827
Bosco Ho <boscojwho@gmail.com>
2024-07-25 01:18:56 -0700

    Initial Commit

---
+++ PyTerm/PyTermApp.swift
@@ -0,0 +1,17 @@
+//
+//  PyTermApp.swift
+//  PyTerm
+//
+//  Created by Bosco Ho on 2024-07-25.
+//
+
+import SwiftUI
+
+@main
+struct PyTermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
